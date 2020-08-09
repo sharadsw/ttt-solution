c68da230a11a08f8ef307cf19de404e823d4391a
TheRealSS <sharad97@gmail.com>
2020-08-09 18:16:55 +0530

    Fix code alignment

--- forms.py
+++ forms.py
@@ -4,6 +4,6 @@ from wtforms.validators import DataRequired, NumberRange
 
 class nForm(FlaskForm):
     num = IntegerField("Input N", 
-        validators=[DataRequired(message="Please enter a number"), 
+            validators=[DataRequired(message="Please enter a number"), 
                 NumberRange(min=1, message="N must be a number greater than 0")])
     submit = SubmitField("Go!")
