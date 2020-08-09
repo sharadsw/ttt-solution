81540b58111a3ffc5510b209bf032b874e4a8854
TheRealSS <sharad97@gmail.com>
2020-08-09 20:40:52 +0530

    Remove hardcoded key

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ from flask import Flask
 import os
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY") or "12345"
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
